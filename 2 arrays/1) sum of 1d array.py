2af216bf405ba9b80101c8f3666a3071e9de9bd5
ZeraAI <parkzen68@gmail.com>
2025-08-07 19:58:43 -0500

    another attempt

--- 2 arrays/1) sum of 1d array.py
+++ 2 arrays/1) sum of 1d array.py
@@ -1,28 +1,23 @@
-# given 1-d array of int
-# create new array representing sum of original array
-# running sum at pos i in new array calculated as sum of all nums in original array from 0th index up to i-th index (inclusive)
-# resulting array computed as --> result [i] = sum(nums[0] + nums[1] + ... + nums[i]) for each i from 0 to length of array minus one
-
-# example:
-# input: [2,3,5,1,6]
-# expect output: [2,5,10,11,17]
-
 class Solution:
     def runningSum(self, nums):
-        result = [0] * len(nums)
+        result = [0] * len(nums)  
         # TODO: Write your code here
-        # [0,0,0,0,0]
+        # [0, 0, 0, 0, 0]
+        # [             ]
         for i, n in enumerate(nums):
             if result[0] != nums[0]:
-                result[0] = nums[0]
+                result[0] = nums[i]
             else:
-                result[i] = result[i] + n + result[i-1]
-
-
-        ###
+                result[i] = result[i - 1] + n
 
         return result
     
+
 if __name__ == "__main__":
-    test = [3,1,4,2,2]
-    print(Solution().runningSum(test)) # [3,4,8,10,12]
+    sample1 = [2, 3, 5, 1, 6]
+    sample2 = [1, 1, 1, 1, 1]
+    sample3 = [-1, 2, -3, 4, -5]
+
+    print(Solution().runningSum(sample1))
+    print(Solution().runningSum(sample2))
+    print(Solution().runningSum(sample3))
